b3e17a46384398ffc2ebcbfbe51b7ea6e5b5db37
Minory54 <olesya.sartina@gmail.com>
2021-10-14 14:45:47 +0700

    Добавление авторизации

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,92 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>{{ config('app.name', 'Laravel') }}</title>
+
+    <!-- Scripts -->
+    <script src="{{ asset('js/app.js') }}" defer></script>
+
+    <!-- Fonts -->
+    <link rel="dns-prefetch" href="//fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
+
+    <!-- Styles -->
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+</head>
+
+<body>
+    <div id="app">
+
+        <!-- Навигационная панель -->
+        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
+            <div class="container">
+
+                <!-- Логотип -->
+                <a class="navbar-brand" href="{{ url('/') }}">Altari</a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+
+                <div class="" id="navbarSupportedContent">
+
+                    <!-- Кнопки авторизации и регистрации -->
+                    <ul class="navbar-nav ml-auto">
+
+                        <!-- Аунтефикация -->
+
+                        <!-- Если пользователь гость, то отображаются две кнопки Авторизация и регистрация -->
+                        @guest
+                            @if (Route::has('login'))
+                                <li class="nav-item">
+                                    <a class="nav-link" href="{{ route('login') }}">{{ __('Авторизация') }}</a>
+                                </li>
+                            @endif
+
+                            @if (Route::has('register'))
+                                <li class="nav-item">
+                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Регистрация') }}</a>
+                                </li>
+                            @endif
+                        @else <!-- Иначе появляется выпадающий список именем -->
+                        <li class="nav-item dropdown">
+
+                            <!-- Отображение имени авторизированного пользователя -->
+                            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                {{ Auth::user()->name }}
+                            </a>
+
+                            <!-- Пункты выпадающего списка -->
+                            <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                                <li><a class="dropdown-item" href="#">Задачи</a></li>
+                                <li>
+                                    <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+                                        {{ __('Выход') }}
+                                    </a>
+                                </li>
+                                <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                                    @csrf
+                                </form>
+                            </ul>
+                        </li>
+                        @endguest
+                    </ul>
+                </div>
+            </div>
+        </nav>
+
+        <main class="py-4">
+            @yield('content')
+
+            @include('inc.footer') <!-- Вставляется блок footer -->
+        </main>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.min.js" integrity="sha384-skAcpIdS7UcVUC05LJ9Dxay8AXcDYfBJqt1CJ85S/CFujBsIzCIv+l9liuYLaMQ/" crossorigin="anonymous"></script>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -14,29 +14,17 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('index');
-}) ->name('index');
-
-Route::get('/login', function () { // Путь к странице
-    return view('login'); // view('название страницы')
-}) ->name('login'); // название функции по которму можно обращаться к этому обработчику
-
-Route::post('/login/submit', function () {
-    return view('person');
-}) ->name('login-form');
-
-Route::get('/register', function () {
-    return view('register');
-}) ->name('register');
-
-Route::post('/register/submit', function () {
-    return view('login');
-}) ->name('register-form');
+Route::get('/', function () {// Путь к странице
+    return view('index');// view('название страницы')
+}) ->name('index');// название функции по которму можно обращаться к этому обработчику
 
 Route::get('/person', function () {
     return view('person');
 }) ->name('person');
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
 // Админская панель
 Route::middleware(['role:admin'])->prefix('admin_panel')->group(function () {
