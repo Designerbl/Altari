baf316acc844e342be1a1a39c4497876266940cb
Minory54 <olesya.sartina@gmail.com>
2021-10-14 21:09:39 +0700

    Внесение небольших правок перед добавлением ролей

--- resources/views/homeAdmin.blade.php
+++ resources/views/homeAdmin.blade.php
@@ -1,14 +1,12 @@
-<!-- Личный кабинет пользователя в который он сможет попасть после авторизации и увидеть список задач по работе с файлами выгрузки (таблицу) -->
-
 @extends('layouts.app') <!-- Наследование кода от основного шаблона с мета данными и прочим -->
-@section('title')Личный кабинет@endsection
+@section('title')Altari - админ-панель@endsection
 
 @section('content') <!-- Блок кода, который подставляется в основной шаблон через команду yield('content') -->
 <div class="container">
     <div class="card border-success">
         <div class="card-body text-success">
             <h5 class="card-title">Готово!</h5>
-            <p class="card-text">Вы успешно авторизированы.</p>
+            <p class="card-text">Вы успешно авторизированы как администратор.</p>
         </div>
     </div>        
 </div>
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Models\UserTask;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
