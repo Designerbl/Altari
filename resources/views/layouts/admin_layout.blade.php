f282d57b52022f2d34c9a25a7559b6234844858a
Minory54 <olesya.sartina@gmail.com>
2021-10-15 17:44:03 +0700

    Исправление бокового меню в админ-панели

--- resources/views/layouts/admin_layout.blade.php
+++ resources/views/layouts/admin_layout.blade.php
@@ -30,102 +30,106 @@
 <body class="hold-transition sidebar-mini layout-fixed">
     <div class="wrapper">
 
-    <!-- Боковая панель -->
-    <aside class="main-sidebar sidebar-dark-primary elevation-4">
-        
-        <!-- Логотип -->
-        <a href="index3.html" class="brand-link">
-            <img src="/admin/dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
-            <span class="brand-text font-weight-light">Админ-панель</span>
-        </a>
+        <!-- Навигационная панель -->
+        <nav class="main-header navbar navbar-expand navbar-white navbar-light">
+            <!-- Left navbar links -->
+            <ul class="navbar-nav">
+              <li class="nav-item">
+                <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
+              </li>
+            </ul>
+        </nav>
 
-        <!-- Боковая панель внутренности -->
-        <div class="sidebar">
+        <!-- Боковая панель -->
+        <aside class="main-sidebar sidebar-dark-primary elevation-4">
+            
+            <!-- Логотип -->
+            <a href="index3.html" class="brand-link">
+                {{-- <img src="/admin/dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8"> --}}
+                <span class="brand-text font-weight-light pl-3 ">Админ-панель</span>
+            </a>
 
-            <!-- Пользователь (админ) -->
-            <div class="user-panel mt-3 pb-3 mb-3 d-flex">
-                <div class="image">
-                    <img src="/admin/dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
+            <!-- Боковая панель внутренности -->
+            <div class="sidebar">
+
+                <!-- Пользователь (админ) -->
+                <div class="user-panel mt-3 pb-3 mb-3 d-flex">
+                    <div class="image">
+                        <img src="/admin/dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
+                    </div>
+
+                    <div class="info">
+                        <a href="#" class="d-block">{{ Auth::user()->name }}</a>
+                    </div>
                 </div>
 
-                <div class="info">
-                    <a href="#" class="d-block">{{ Auth::user()->name }}</a>
-                </div>
+                <!-- Sidebar Menu -->
+                <nav class="mt-2">
+                    <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+                        
+                        <li class="nav-item">
+                            <a href=" {{ route('homeAdmin') }}" class="nav-link">
+                            <i class="nav-icon fas fa-tachometer-alt"></i>
+                            <p>Главная страница</p>
+                            </a>
+                        </li>
+
+                        <li class="nav-item">
+                            <a href="#" class="nav-link">
+                                <i class="nav-icon fas fa-user"></i>
+                                <p>
+                                    Пользователи
+                                    <i class="right fas fa-angle-left"></i>
+                                </p>
+                            </a>
+                            <ul class="nav nav-treeview">
+                                <li class="nav-item">
+                                    <a href="./index.html" class="nav-link">
+                                        <p>Все пользователи</p>
+                                    </a>
+                                </li>
+                            </ul>
+                        </li>
+
+                        
+                        <li class="nav-item">
+                            <a href="#" class="nav-link">
+                                <i class="nav-icon fas fa-calendar-check"></i>
+                                <p>
+                                    Задачи
+                                    <i class="right fas fa-angle-left"></i>
+                                </p>
+                            </a>
+                            <ul class="nav nav-treeview">
+                                <li class="nav-item">
+                                    <a href="./index.html" class="nav-link">
+                                        <p>Все задачи</p>
+                                    </a>
+                                </li>
+                            </ul>
+                        </li>
+                    </ul>
+                </nav>
+            <!-- /.sidebar-menu -->
             </div>
+            <!-- /.sidebar -->
+        </aside>
 
-            <!-- Sidebar Menu -->
-            <nav class="mt-2">
-                <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
-                    
-                    <li class="nav-item">
-                        <a href=" {{ route('homeAdmin') }}" class="nav-link">
-                          <i class="nav-icon fas fa-tachometer-alt"></i>
-                          <p>Главная страница</p>
-                        </a>
-                    </li>
-
-                    <li class="nav-item">
-                        <a href="#" class="nav-link">
-                            <i class="nav-icon fas fa-user"></i>
-                            <p>
-                                Пользователи
-                                <i class="right fas fa-angle-left"></i>
-                            </p>
-                        </a>
-                        <ul class="nav nav-treeview">
-                            <li class="nav-item">
-                                <a href="./index.html" class="nav-link">
-                                    <p>Все пользователи</p>
-                                </a>
-                            </li>
-                        </ul>
-                    </li>
-
-                    
-                    <li class="nav-item">
-                        <a href="#" class="nav-link">
-                            <i class="nav-icon fas fa-calendar-check"></i>
-                            <p>
-                                Задачи
-                                <i class="right fas fa-angle-left"></i>
-                            </p>
-                        </a>
-                        <ul class="nav nav-treeview">
-                            <li class="nav-item">
-                                <a href="./index.html" class="nav-link">
-                                    <p>Все задачи</p>
-                                </a>
-                            </li>
-                        </ul>
-                    </li>
-                </ul>
-            </nav>
-        <!-- /.sidebar-menu -->
+        <!-- Контент страницы -->
+        <div class="content-wrapper">
+            @yield('content')
         </div>
-        <!-- /.sidebar -->
-    </aside>
 
-    <!-- Контент страницы -->
-    <div class="content-wrapper">
-        @yield('content')
+        <!-- Подвал -->
+        {{-- <footer class="main-footer">
+            <strong>Copyright &copy; 2014-2021 <a href="https://adminlte.io">AdminLTE.io</a>.</strong>
+            All rights reserved.
+            <div class="float-right d-none d-sm-inline-block">
+            <b>Version</b> 3.1.0
+            </div>
+        </footer> --}}
+
     </div>
-
-    <!-- Подвал -->
-    {{-- <footer class="main-footer">
-        <strong>Copyright &copy; 2014-2021 <a href="https://adminlte.io">AdminLTE.io</a>.</strong>
-        All rights reserved.
-        <div class="float-right d-none d-sm-inline-block">
-        <b>Version</b> 3.1.0
-        </div>
-    </footer> --}}
-
-    <!-- Control Sidebar -->
-    <aside class="control-sidebar control-sidebar-dark">
-        <!-- Control sidebar content goes here -->
-    </aside>
-    <!-- /.control-sidebar -->
-    </div>
-    <!-- ./wrapper -->
 
     <!-- jQuery -->
     <script src="/admin/plugins/jquery/jquery.min.js"></script>
